348eb591d75608097ea914e5ee6a0c90f318d4a8
ZITANIMohamed98 <zitaniceo@gmail.com>
2023-08-12 19:36:07 +0200

    start commit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,28 @@
-const Discover = () => <div>Discover</div>;
+import {Error, Loader, SongCard} from '../components';
+import { genres}  from '../assets/constants';
 
+const Discover = () => {
+    console.log(genres);
+    const genreTitle = 'Pop';
+    return (
+    <div className='flex flex-col'>
+        <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
+            <h2 className='font-bold text-3x1 text-white text-left'>Discover {genreTitle}</h2>
+            <select
+                onChange={() => {}}
+                value=""
+                className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
+            >
+                {genres.map(genre => (
+                    <option key={genre.value} value={genre.value}>{genre.title}</option>
+                ))}
+            </select>
+        </div>
+
+        <div className='flex flex-wrap sm:flex-start.'>
+
+        </div>
+    </div>
+    );
+};
 export default Discover;
